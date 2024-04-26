ed372fb29048baabccd83251d29c166bf23cd837
Dio Siregar <dio.siregar03@gmail.com>
2024-04-26 10:32:06 +0700

    THE END

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -12,13 +12,12 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->uuid('id')->primary("")->default("-");
+            $table->uuid('id')->primary()->unique();
             $table->string('nama');
             $table->string('email')->unique();
             $table->string('password');
             $table->string('deskripsi')->default("-");
             $table->date('tanggal_lahir');
-            $table->string('name')->nullable();
             $table->rememberToken()->nullable();
             $table->timestamps();
         });
